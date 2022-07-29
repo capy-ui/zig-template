cbb4ad688b0fd82285301dbe0bbf9ba152292586
Zen1th <39484230+zenith391@users.noreply.github.com>
2022-07-29 17:52:33 +0000

    Update main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,20 @@
 const std = @import("std");
-const zgt = @import("zgt");
+const capy = @import("capy");
 
 // This is required for youe app to build to WebAssembly and other particular architectures
-pub usingnamespace zgt.cross_platform;
+pub usingnamespace capy.cross_platform;
 
 pub fn main() !void {
-    try zgt.backend.init();
+    try capy.backend.init();
     
-    var window = try zgt.Window.init();
+    var window = try capy.Window.init();
     try window.set(
-        zgt.Label(.{ .text = "Hello, World" })
+        capy.Label(.{ .text = "Hello, World" })
     );
     
     window.setTitle("Hello");
     window.resize(250, 100);
     window.show();
     
-    zgt.runEventLoop();
+    capy.runEventLoop();
 }

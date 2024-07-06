ecb403107526c99074cbb14ceb256e5056f0fa78
zenith391 <39484230+zenith391@users.noreply.github.com>
2024-07-06 23:51:52 +0200

    update to 2024.5.0-mach version

--- build.zig
+++ build.zig
@@ -1,39 +1,47 @@
 const std = @import("std");
-const capy = @import("capy");
+const build_capy = @import("capy");
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const capy_dep = b.dependency("capy", .{
+        .target = target,
+        .optimize = optimize,
+        .app_name = @as([]const u8, "capy-template"),
+    });
+    const capy = capy_dep.module("capy");
+
     const exe = b.addExecutable(.{
         .name = "capy-template",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
+    exe.root_module.addImport("capy", capy);
     b.installArtifact(exe);
 
-    const run_cmd = try capy.install(exe, .{ .args = b.args });
+    const run_cmd = try build_capy.runStep(exe, .{ .args = b.args });
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(run_cmd);
 
     // Building for WebAssembly
-    // WebAssembly doesn't have a concept of executables, so the way it works is that we make a shared library and capy exports a '_start' function automatically
     @setEvalBranchQuota(5000);
     const wasm = b.addExecutable(.{
         .name = "capy-template",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = b.resolveTargetQuery(
             comptime std.Target.Query.parse(.{ .arch_os_abi = "wasm32-freestanding" }) catch unreachable,
         ),
         .optimize = optimize,
     });
-    const serve = try capy.install(wasm, .{});
+    wasm.root_module.addImport("capy", capy);
+    const serve = try build_capy.runStep(wasm, .{});
     const serve_step = b.step("serve", "Start a local web server to run this application");
     serve_step.dependOn(serve);
 
     const exe_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
